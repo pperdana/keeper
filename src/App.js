bb10b5381e08fe209a630aca7a1c26dd98b8fe0e
Panji <panji9502@gmail.com>
2023-01-10 20:56:19 +0700

    add components and its functionallity

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import Header from "./components/header.component";
+import Footer from "./components/footer.component";
+import Note from "./components/note.component";
+import CreateArea from "./components/create-area.component";
+import { useState } from "react";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [notes, setNotes] = useState([]);
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
